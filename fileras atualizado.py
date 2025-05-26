ff695e53693e5778a516613a1a8055bc0205a1a6
miguel-sccp <miguel.reis@hotmail.com.br>
2025-05-25 23:08:23 -0300

    Add files via upload

---
+++ fileras atualizado.py
@@ -0,0 +1,18 @@
+print("Seja bem-vindo")
+numero = []
+
+for i in range(5):
+    while True:
+        entrada = input(f"Digite o {i+1}º número: ")
+        try:
+            valor = int(entrada)
+            numero.append(valor)
+            break
+        except ValueError:
+            print("Digite apenas números inteiros idiota.")
+
+print("Lista:", numero)
+print("Maior número:", max(numero))
+print("Menor número:", min(numero))
+print("Soma de todos os números:", sum(numero))
+print("Quantidade de números no total:", len(numero))
